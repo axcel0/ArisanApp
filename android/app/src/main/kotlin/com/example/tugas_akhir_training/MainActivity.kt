5106645f1a90f1dfb8d06ddbf084e6787514674d
Axel Santoso <axcelsantoso0@gmail.com>
2023-09-13 15:58:15 +0700

    add shared pref dependencies

--- android/app/src/main/kotlin/com/example/tugas_akhir_training/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tugas_akhir_training/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tugas_akhir_training
 
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
 }
