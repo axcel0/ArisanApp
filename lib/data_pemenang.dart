30c39ef50deb9564472d528e1f2c5cb273eb4508
Bima <bimanugraha732@gmail.com>
2023-09-15 11:12:32 +0700

    add daftar pemenang ui
    add daftar pemenang ui (card listview)

--- lib/data_pemenang.dart
+++ lib/data_pemenang.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:tugas_akhir_training/nama_peserta.dart';
+
 class DataPemenang extends StatelessWidget {
-  const DataPemenang({super.key});
+  DataPemenang({super.key});
+
+  List<NamaPeserta> daftarNamaMenang = [
+    NamaPeserta(idPeserta: "0", namaPeserta: "a"),
+    NamaPeserta(idPeserta: "1", namaPeserta: "b"),
+    NamaPeserta(idPeserta: "2", namaPeserta: "c"),
+    NamaPeserta(idPeserta: "3", namaPeserta: "d"),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Provider Demo'),
+        title: const Text('Pemenang Arisan'),
         //add theme to this appbar
         backgroundColor: Theme.of(context).colorScheme.primary,
-
       ),
-      body: const Center(),
+      body: ListView.builder(
+        itemCount: daftarNamaMenang.length,
+        itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            title: Text(daftarNamaMenang[index].namaPeserta),
+            subtitle: Text(daftarNamaMenang[index].idPeserta),
+          ),
+        );
+      }),
     );
   }
 }
